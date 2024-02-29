b902495d72bc803611303c9932f7c6db3c93ccfa
Nolan Birkeland <nolan.birkeland@gmail.com>
2024-02-29 10:50:53 -0800

    added some new remaps

--- lua/njbirk/core/keymaps.lua
+++ lua/njbirk/core/keymaps.lua
@@ -12,4 +12,9 @@ keymap.set("i", "jk", "<ESC>")
 -- plugins, opts- auto-centering for various things
 keymap.set("n", "<C-d>", "<C-d>zz")
 keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
 
+-- move blocks of text in visual mode
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
