77d0db8c182a7b93b0c8c5170ada00fff260d195
Nolan Birkeland <nolan.birkeland@gmail.com>
2024-03-23 16:40:01 -0400

    adds basic lsp setup

--- lua/njbirk/plugins/lsp-config.lua
+++ lua/njbirk/plugins/lsp-config.lua
@@ -1,30 +1,29 @@
-return
-{
+return {
 	{
-			"williamboman/mason.nvim",
-			config = function()
-				require("mason").setup()
-			end
+		"williambowman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
 	},
 	{
-			"williamboman/mason-lspconfig.nvim",
-			config = function()
-				require("mason-lspconfig").setup({
-					ensure_installed = { 'lua_ls', 'clangd' }
-				})
-				require("mason-lspconfig").setup_handlers {
-					function(server_name)
-						require('lspconfig')[server_name].setup {}
-					end
-				}
-			end
+		"williambowman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { 'lua_ls' }
+			})
+		end
 	},
 	{
-			"neovim/nvim-lspconfig",
-			config = function()
-				local lspconfig = require('lspconfig')
-				vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-				vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
-			end
-	},
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			vim.keymap.set(opt, 'K', vim.lsp.buf.hover, {})
+			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+    	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+			vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, {})
+    	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+    	vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
+		end
+	}
 }
