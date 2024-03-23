69fc83fbb9a9ede87a4690969073e5f73d949fde
Nolan Birkeland <nolan.birkeland@gmail.com>
2024-03-23 19:24:07 -0400

    lsp properly working now

--- lua/njbirk/plugins/lsp-config.lua
+++ lua/njbirk/plugins/lsp-config.lua
@@ -9,21 +9,36 @@ return {
 		"williambowman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { 'lua_ls' }
+				ensure_installed = { 'lua_ls' },
+				automatic_installation = true
 			})
 		end
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			vim.keymap.set(opt, 'K', vim.lsp.buf.hover, {})
-			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-    	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, {})
-    	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
-    	vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
+			-- Setup language servers.
+			local lspconfig = require('lspconfig')
+			lspconfig.pyright.setup {}
+			lspconfig.tsserver.setup {}
+			lspconfig.clangd.setup {}
+
+			vim.api.nvim_create_autocmd('LspAttach', {
+				group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+				callback = function(ev)
+				vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+				local opts = { buffer = ev.buf }
+				vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+				vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+				vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+				vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+				vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+				vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+				vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+				vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  		end, 
+			})
 		end
 	}
 }
