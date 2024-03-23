d83f3a5bc967944ec01b85f8e4d19c88ddde3bac
Nolan Birkeland <nolan.birkeland@gmail.com>
2024-03-23 19:44:08 -0400

    adds formatting and linting with none-ls

--- lua/njbirk/plugins/treesitter.lua
+++ lua/njbirk/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return
     config = function () 
     	local configs = require("nvim-treesitter.configs")
 			configs.setup({
-      ensure_installed = { "c", "lua", "cpp", "python", "json" },
+			auto_install = true,
       sync_install = false,
 			highlight = { enable = true },
       indent = { enable = true },  
