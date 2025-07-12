f6ea96c7152f4e0927848d83a9d384d0564dc575
Nolan Birkeland <92126122+njbirk@users.noreply.github.com>
2025-07-11 22:19:59 -0700

    changes treesitter context to context.vim (just works better lol)

--- lua/njbirk/plugins/treesitter.lua
+++ lua/njbirk/plugins/treesitter.lua
@@ -1,30 +1,28 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        highlight = {
-          enable = true,
-        },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				highlight = {
+					enable = true,
+				},
 
-        -- enable indentation
-        indent = { enable = true },
+				-- enable indentation
+				indent = { enable = true },
 
-        -- autotagging
-        autotag = {
-          enable = true,
-        },
+				-- autotagging
+				autotag = {
+					enable = true,
+				},
 
-        auto_install = true,
-        sync_install = false,
-      })
-    end,
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-context',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    event = 'BufReadPre',
-  },
+				auto_install = true,
+				sync_install = false,
+			})
+		end,
+	},
+	{
+		"wellle/context.vim",
+	},
 }
